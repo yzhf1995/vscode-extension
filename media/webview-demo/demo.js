fc04057f362586e87e94104ca239f816520b273b
yzhf1995 <yzhf1995@163.com>
2021-05-05 21:36:37 +0800

    add high chart config

--- media/webview-demo/demo.js
+++ media/webview-demo/demo.js
@@ -43,7 +43,7 @@ const subLayerConfig = [{
         //General
         content: [ {                
             type: 'component',
-            componentName: 'main-layout',
+            componentName: 'stock-chart',
             componentState: { label: 'value' },
             id: '101',
             width: 10,
@@ -188,6 +188,62 @@ $().ready( () => {
           return area;
         };
       });
+
+
+    StockChartComponent = function( container, state ) {
+        this._highChartsConfig = {
+            title: { text: 'My First chart' },
+            series: [{                              // 数据列
+                name: '小明',                        // 数据列名
+                data: [1, 0, 4]                     // 数据
+            }, {
+                name: '小红',
+                data: [5, 7, 3]
+            }],
+            plotOptions: { line: { marker: { enabled: false } } },
+            xAxis:{categories: ['苹果', '香蕉', '橙子']  },
+            yAxis:{ 
+                text: '吃水果个数'                // y 轴标题
+            },
+            chart:{
+                type: 'bar',
+                renderTo: container.getElement()[ 0 ] 
+            }
+        };
+    
+        this._container = container;
+        this._state = state;
+        this._chart = null;
+    
+        this._container.setTitle( 'First Chart' );
+        this._container.on( 'open', this._createChart.bind( this ) );
+    };
+    
+    StockChartComponent.prototype._createChart = function() {
+        this._chart = new Highcharts.Chart( this._highChartsConfig );
+    
+        this._chart.hideLoading();
+        this._bindContainerEvents();
+    };
+    
+    StockChartComponent.prototype._onDataReady = function( highchartsData ) {
+        this._chart.addSeries({
+            color: this._state.color,
+            name: this._state.companyName,
+            data: highchartsData
+        });
+    };
+    
+    StockChartComponent.prototype._bindContainerEvents = function() {
+        this._container.on( 'resize', this._setSize.bind( this ) );
+        this._container.on( 'destroy', this._chart.destroy.bind( this._chart ) );
+    };
+    
+    StockChartComponent.prototype._setSize = function() {
+        this._chart.setSize( this._container.width, this._container.height );
+    };
+
+    layout.registerComponent( 'stock-chart', StockChartComponent);   
 
     layout.init();
 });
